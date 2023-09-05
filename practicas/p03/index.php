9fd9508dc960b176091c5370a84006404e0ec03b
NeoFG <fernando.garza1@alumno.buap.mx>
2023-09-05 13:27:56 -0600

    Este es el commit del ejercicio 2 terminado de la practica 3

--- practicas/p03/index.php
+++ practicas/p03/index.php
@@ -33,6 +33,50 @@
         echo '<li>$house*5 es invalida porque no se realiza una operacion</li>';
         echo '</ul>';
     ?>
+    <h2>Ejercicio 2</h2>
+    <p>Proporcionar los valores de $a, $b, $c como sigue:</p>
+    <li>$a = "ManejadorSQL";</li>
+    <li>$b = 'MySQL';</li>
+    <li>$c = &$a;</li>
+
+    <h3>a. Ahora muestra el contenido de cada variable</h3>
+    <?php
+        $a = "ManejadorSQL";
+        $b = 'MySQL';
+        $c = &$a;
+        echo ("Primer variable: ").$a;
+        echo '<br>';
+        echo ("Segunda variable: ").$b;
+        echo '<br>';
+
+        // Imprimimos el contenido de las variables
+        echo ("Tercera variable hace referencia a la primera: ").$c;
+
+        echo '<p>La primier varibale tiene asignada contenido string</p>';
+
+        echo '<p>La segunda varibale tiene asignada contenido string</p>';
+        
+        echo '<p>La tercera variable tiene asignada la direccion de memoria de memoria de la variable 2 por lo que imprime su contenido string</p>';
+
+        // Cambiamos el contenido de las variables anteriores
+        $a = "PHP server";
+        $b = &$a; // Ahora hace referencia a la dirreccion de memoria de la var a
+        
+        echo '<h3>b. Agrega al código actual las siguientes asignaciones:</h3>';
+        echo '$a = “PHP server”; $b = &$a;';
+
+        // Parte del ejecicio 2 inciso c
+        echo '<h3>c. Vuelve a mostrar el contenido de cada uno </h3>';
+        echo ("Contenido modificado de la variable a: ").$a;
+        echo '<br>';
+        echo ("Contenido modificado de la variable b: ").$b;
+
+        echo '<h3>d. Describe en y muestra en la página obtenida qué ocurrió en el segundo bloque de asignaciones </h3>';
+        echo ("Lo que ocurrio fue que a la variable $ a se le cambio el contenido y lo cambiamos a: ").$a;
+        echo '<br>';
+        echo ("Lo que ocurrio en la variable $ b fue que utilizamos un apuntador hacia la variable b por lo que su contenido cambio al contenido de la variable a: ").$b;
+    ?>
+
     
 </body>
 </html>
