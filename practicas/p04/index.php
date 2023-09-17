8c6f7c1a292274cf8cf64034715c63a8a2846254
NeoFG <fernando.garza1@alumno.buap.mx>
2023-09-17 17:53:24 -0600

    Primer ejercicio multiplo de 5 y 7 ahora con el formato XHTML y en una funcion con archivo aparte completado

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -1,28 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>Práctica 4</title>
 </head>
 <body>
     <h2>Ejercicio 1</h2>
     <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
+
+    <form method="get" action="index.php">
+        <label for="numero">Ingrese un número:</label>
+        <input type="number" name="numero" id="numero">
+        <input type="submit" value="Verificar">
+    </form>
+
     <?php
-        if(isset($_GET['numero']))
-        {
+        // Incluimos el archivo con la función nos trae todo el php.
+        include('eje1.php'); 
+
+        if (isset($_GET['numero'])) {
             $num = $_GET['numero'];
-            if ($num%5==0 && $num%7==0)
-            {
-                echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
-            }
-            else
-            {
-                echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
-            }
+            // Llama a la función con el número que pusimos.
+            verificarMultiplo($num); 
         }
     ?>
-
 
 </body>
 </html>
