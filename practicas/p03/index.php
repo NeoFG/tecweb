5279d003794968d72856226c97d6f26608086cab
NeoFG <fernando.garza1@alumno.buap.mx>
2023-09-22 21:27:07 -0600

    Practica P03 Validada en Markup Validation Service y completando practica p06 con exito.

--- practicas/p03/index.php
+++ practicas/p03/index.php
@@ -1,12 +1,16 @@
-<!DOCTYPE html PUBLIC “-//W3C//DTD XHTML 1.1//EN”
-“http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd”>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>Document</title>
 </head>
 <body>
+    <p>
+        <a href="http://validator.w3.org/check?uri=referer"><img
+        src="http://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" /></a>
+    </p>
     <h2>Ejercicio 1</h2>
     <p>1.Determina cuál de las siguientes variables son válidas y explica por qué:</p>
     <p>$_myvar, $_7var, myvar, $myvar, $var7, $_element1, $house*5</p>
@@ -38,7 +42,7 @@
     <ul>
         <li>$a = "ManejadorSQL";</li>
         <li>$b = 'MySQL';</li>
-        <li>$c = &$a;</li>
+        <li>$c = &amp;$a;</li>
     </ul>
 
     <h3>a. Ahora muestra el contenido de cada variable</h3>
@@ -83,7 +87,7 @@
     <p>3.Muestra el contenido de cada variable inmediatamente después de cada asignación, verificar la evolución del tipo de estas variables (imprime todos los componentes de los arreglo)</p>
     <ol>
         <li>$a = "PHP5";    </li>
-        <li>$z[] = &$a;     </li>
+        <li>$z[] = &amp;$a;</li>
         <li>$b = "5a version de PHP";</li>
         <li>$c = $b*10;     </li>
         <li>$a .= $b;       </li>
